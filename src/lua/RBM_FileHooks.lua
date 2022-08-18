26b9432e8c83300a2f98559bf81afcd0466869ad
adsfgg <11364579+adsfgg@users.noreply.github.com>
2022-08-18 18:56:16 +0100

    Add CompMod Shotgun changes

--- src/lua/RBM_FileHooks.lua
+++ src/lua/RBM_FileHooks.lua
@@ -1,7 +1,13 @@
 g_rbm_revision = 1
 
+-- Player Classes
+ModLoader.SetupFileHook("lua/Player_Client.lua", "lua/RBM/Classes/Player/Player_Client.lua", "post")
+
+-- Globals
+ModLoader.SetupFileHook("lua/Balance.lua", "lua/RBM/Globals/Balance.lua", "post")
+
 -- GUI
 ModLoader.SetupFileHook("lua/GUIFeedback.lua", "lua/RBM/GUI/GUIFeedback.lua", "post")
 
--- Player Classes
-ModLoader.SetupFileHook("lua/Player_Client.lua", "lua/RBM/Classes/Player/Player_Client.lua", "post")
+-- Weapons
+ModLoader.SetupFileHook("lua/Weapons/Marine/Shotgun.lua", "lua/RBM/Weapons/Marine/Shotgun.lua", "post")
