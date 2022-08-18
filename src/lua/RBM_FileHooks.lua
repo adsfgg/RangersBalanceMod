a778b6c4675d3603c55087d0575824a49758c776
adsfgg <11364579+adsfgg@users.noreply.github.com>
2022-08-18 20:37:38 +0100

    Remove Onos

--- src/lua/RBM_FileHooks.lua
+++ src/lua/RBM_FileHooks.lua
@@ -9,5 +9,11 @@ ModLoader.SetupFileHook("lua/Balance.lua", "lua/RBM/Globals/Balance.lua", "post"
 -- GUI
 ModLoader.SetupFileHook("lua/GUIFeedback.lua", "lua/RBM/GUI/GUIFeedback.lua", "post")
 
+-- Teams
+ModLoader.SetupFileHook("lua/AlienTeam.lua", "lua/RBM/Teams/AlienTeam.lua", "post")
+
+-- Tech
+ModLoader.SetupFileHook("lua/AlienTechMap.lua", "lua/RBM/Tech/AlienTechMap.lua", "post")
+
 -- Weapons
 ModLoader.SetupFileHook("lua/Weapons/Marine/Shotgun.lua", "lua/RBM/Weapons/Marine/Shotgun.lua", "post")
