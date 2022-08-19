284eb5a853f9dbb2c9ac3bb2090e6462956fd140
adsfgg <11364579+adsfgg@users.noreply.github.com>
2022-08-19 10:23:07 +0100

    Increase Shotgun cost by 5

--- src/lua/RBM/Globals/Balance.lua
+++ src/lua/RBM/Globals/Balance.lua
@@ -1,9 +1,13 @@
--- Increase damage per upgrade to ~13.33 from ~10
+-- Lifeforms
+kLerkHealth = 200 -- 180
+kFadeHealth = 300 -- 250
 
+-- Weapons
+
+kShotgunCost = 25 -- 20
+
+-- Increase damage per upgrade to ~13.33 from ~10
 local kShotgunDamagePerUpgradeScalar = 0.078
 kShotgunWeapons1DamageScalar = 1 + kShotgunDamagePerUpgradeScalar
 kShotgunWeapons2DamageScalar = 1 + kShotgunDamagePerUpgradeScalar * 2
 kShotgunWeapons3DamageScalar = 1 + kShotgunDamagePerUpgradeScalar * 3
-
-kLerkHealth = 200 -- 180
-kFadeHealth = 300 -- 250
